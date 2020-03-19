5a873ee195f248a5cbb89f3c2df4f6ae0a459849
subnub <kyle.hoell@gmail.com>
2020-03-19 19:20:22 -0400

    updated

--- src/projectsInfo/index.js
+++ src/projectsInfo/index.js
@@ -4,7 +4,7 @@ export default {
     myDriveTitle: "MyDrive",
     myDriveDesc: "MyDrive is a Node.js, React, and Mongo.db Google Drive Clone.",
     myDriveGit: "https://github.com/subnub/myDrive",
-    myDriveImage: "../images/mydrive.png",
+    myDriveImage: "https://raw.githubusercontent.com/subnub/subnub.github.io/master/build/images/mydrive.png",
     myDriveLongDesc: "MyDrive is an Open-Source full-stack application I created. MyDrive clearly demonstrates my ability to break applications into small coherent pieces. MyDrive is relatively popular on Github, receiving over 800 stars, 100 forks, and 24,000 views.",
     myDriveDemo: "https://mydrive-demo.herokuapp.com/",
 
@@ -12,7 +12,7 @@ export default {
     expensifyTitle: "Expensify",
     expensifyDesc: "Expensify is a React and Redux based Expense tracker.",
     expensifyGit: "https://github.com/subnub/react-course-expensivify",
-    expensifyImage: "../images/expense.png",
+    expensifyImage: "https://raw.githubusercontent.com/subnub/subnub.github.io/master/build/images/expense.png",
     expensifyLongDesc: "Expenisify is based on React, and Redux, and is a way to manage all of your expenses. You can edit, create, and remove expenses. Expensify also uses Firebase as a backend.",
     expensifyDemo: "https://react-course-expensivify.herokuapp.com/",
 
@@ -20,21 +20,21 @@ export default {
     portfolioTitle: "My Web Portfolio",
     portfolioDesc: "My Web Portfolio is based on React, and Redux.",
     portfolioGit: "",
-    portfolioImage: "../images/portfolio.png",
+    portfolioImage: "https://raw.githubusercontent.com/subnub/subnub.github.io/master/build/images/portfolio.png",
     portfolioLongDesc: "My Web Portfolio demonstrates my ability to build an elegant looking Web Application, it uses React, and Redux",
 
     taskColor: "#f16a55",
     taskTitle: "Task Manager",
     taskDesc: "Task Manager is a Node.js, Express and MongoDB, based backend API for creating tasks",
     taskGit: "https://github.com/subnub/task-manager-api",
-    taskImage: "../images/task.png",
+    taskImage: "https://raw.githubusercontent.com/subnub/subnub.github.io/master/build/images/task.png",
     taskLongDesc: "Task Manger is baed on Node.js, Express, and MongoDB. It supports API calls for creating, updating, and deleting task, as well as a simple web UI.",
 
     syncColor: "#fe1f7c",
     syncTitle: "SyncMessage",
     syncDesc: "Syncmessage is an Android, and Electron application, it sends and receives iMessages",
     syncGit: "https://github.com/subnub/syncmessage-macos-final-public",
-    syncImage: "../images/sync.png",
+    syncImage: "https://raw.githubusercontent.com/subnub/subnub.github.io/master/build/images/sync.png",
     syncLongDesc: "Syncmessage is an Android Application, with the unique ability to send and receive Apple iMessages. It uses Java-Android for the Android App, Electon for the macOS app, and Firebase for the backend.",
     syncDemo: "https://www.syncmessage-android.com/"
 }
